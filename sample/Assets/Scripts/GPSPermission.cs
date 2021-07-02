9cf8f2a99aef852bc1ef3372c5352eb78e82bffb
Canadienwolf <Canadienwolf@gmail.com>
2021-07-02 10:43:35 +0200

    a

--- sample/Assets/Scripts/GPSPermission.cs
+++ sample/Assets/Scripts/GPSPermission.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GPSPermission : MonoBehaviour
 {
-    private void Start()
-    {
-        StartCoroutine(AskGPS());
-    }
+    
 
-    IEnumerator AskGPS()
+    IEnumerator Start()
     {
+        Debug.Log("dingy1");
+
         // First, check if user has location service enabled
         if (!Input.location.isEnabledByUser)
             yield break;
@@ -43,6 +43,7 @@ public class GPSPermission : MonoBehaviour
         {
             // Access granted and location value could be retrieved
             print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            SceneManager.LoadScene(1);
         }
 
         // Stop service if there is no need to query location updates continuously
